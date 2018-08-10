ec52f4827a3db043042de93c98a2ccc8f850af8e
anigania <kunjean4324@gmail.com>
2018-08-10 06:13:14 +0530

    Added data vaildation to User model

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,33 +6,31 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "ruby on rails sample app"
   end
 
-
-
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get  help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
     end
 
   test "should get about" do
-    get static_pages_about_url
+    get  about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
   test "should get contact" do
-    get static_pages_contact_url
+    get  contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
-  test "should get signup" do
-    get static_pages_contact_url
-    assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
-  end
+  #test "should get signup" do
+  #  get  signup_path
+  #  assert_response :success
+  #  assert_select "title", "Contact | #{@base_title}"
+  #end
 end
